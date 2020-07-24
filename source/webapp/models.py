2ed80e7a768c9e553814333de19efad6172d0cc4
violettalivada <vilkalivada@gmail.com>
2020-07-24 06:11:31 +0300

    Исправлена модель и добавлена новая миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,7 +7,7 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class Task(models.Model):
     description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
-    date = models.DateField(auto_now_add=True, verbose_name='Дата выполнения')
+    date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.description)
