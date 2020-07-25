4b9ed990206b844ff058e57541ae247c45d3ec3e
violettalivada <vilkalivada@gmail.com>
2020-07-25 11:05:22 +0300

    Добавлено новое поле 'title' и поле 'description' теперь может быть пустым.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,7 +5,8 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class Task(models.Model):
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    title = models.CharField(max_length=200, null=False, blank=False, default='Task', verbose_name='Заголовок')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
     status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
     date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
 
